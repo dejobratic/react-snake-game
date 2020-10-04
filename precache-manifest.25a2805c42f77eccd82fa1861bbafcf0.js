007f54c1f1bce45eb437113da345a1edc70d7b38
dejobratic <dejobratic@gmail.com>
2020-10-04 12:13:47 +0200

    Updates

---
+++ precache-manifest.25a2805c42f77eccd82fa1861bbafcf0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cdd1cc6edb95faa4c217b27f3f9d633",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "efe944390b271713e01a",
+    "url": "/react-snake-game/static/css/main.401121ca.chunk.css"
+  },
+  {
+    "revision": "bdee4e97e6c6c7edf6df",
+    "url": "/react-snake-game/static/js/2.108c2f68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snake-game/static/js/2.108c2f68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efe944390b271713e01a",
+    "url": "/react-snake-game/static/js/main.94d21743.chunk.js"
+  },
+  {
+    "revision": "29e856bcd72f0474f31a",
+    "url": "/react-snake-game/static/js/runtime-main.0f2d89e1.js"
+  },
+  {
+    "revision": "e6c4427c52b868d9ffa726b862c21113",
+    "url": "/react-snake-game/static/media/level-background.e6c4427c.jpg"
+  },
+  {
+    "revision": "f20532e30a3b3f26d20b846a6f55d256",
+    "url": "/react-snake-game/static/media/snake-graphics.f20532e3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-app/react-snake-game/precache-manifest.824baf29232b612ffc4b5cf63baa7f82.js"
+  "/react-snake-game/precache-manifest.25a2805c42f77eccd82fa1861bbafcf0.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-app/react-snake-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-snake-game/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
